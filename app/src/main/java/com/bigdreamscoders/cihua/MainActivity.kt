07d816aaf7ae94992fb9606e840483586990e01e
TheAlexBig <00062816@uca.edu.sv>
2018-10-26 23:14:25 -0600

    Lo mismo

--- app/src/main/java/com/bigdreamscoders/cihua/MainActivity.kt
+++ app/src/main/java/com/bigdreamscoders/cihua/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.bigdreamscoders.cihua
 
+import android.app.Activity
+import android.app.Fragment
+import android.content.Intent
 import android.graphics.Point
 import android.net.Uri
 import android.os.Bundle
@@ -18,119 +21,14 @@ import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var fragment: ArFragment
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
-
-        fab.setOnClickListener {
-            addObject()
+        callanimation.setOnClickListener {
+            startActivity(Intent(this.baseContext, AnimationActivity::class.java))
         }
     }
 
-    private fun addObject() {
-        val frame = fragment.arSceneView.arFrame
-        val point = getScreenCenter()
-        var elements =  listOf("Heart.sfb", "model.sfb")
-        if (frame != null ) {
-            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
-            for (hit in hits) {
-                val trackable = hit.trackable
-                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
-                    placeFloor(fragment, hit.createAnchor(), Uri.parse(elements.get(1)))
-                    placeObject(fragment, hit.createAnchor(), Uri.parse(elements.get(0)))
-                    break
-                }
-            }
-
-        }
-    }
-    private fun placeObject(fragment: ArFragment, createAnchor: Anchor, model: Uri) {
-        ModelRenderable.builder()
-            .setSource(fragment.context, model)
-            .build()
-            .thenAccept {
-                addRotatingNodeToScene(fragment, createAnchor, it)
-            }
-            .exceptionally {
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage(it.message)
-                    .setTitle("error!")
-                val dialog = builder.create()
-                dialog.show()
-                return@exceptionally null
-            }
-    }
-
-    private fun placeFloor(fragment: ArFragment,createAnchor: Anchor, model: Uri){
-        ModelRenderable.builder()
-            .setSource(fragment.context, model)
-            .build()
-            .thenAccept {
-                addStaticNodeToScene(fragment, createAnchor, it)
-            }
-            .exceptionally {
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage(it.message)
-                    .setTitle("error!")
-                val dialog = builder.create()
-                dialog.show()
-                return@exceptionally null
-            }
-    }
-
-    private fun addStaticNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable){
-        val anchorNode = AnchorNode(createAnchor)
-        val transformableNode = TransformableNode(fragment.transformationSystem)
-        val staticNode = StaticNode()
-        staticNode.renderable = renderable
-        staticNode.addChild(transformableNode)
-        staticNode.setParent(anchorNode)
-
-        fragment.arSceneView.scene.addChild(anchorNode)
-        transformableNode.select()
-    }
-    private fun addRotatingNodeToScene(fragment: ArFragment, createAnchor: Anchor, renderable: ModelRenderable) {
-        val anchorNode = AnchorNode(createAnchor)
-        val transformableNode = TransformableNode(fragment.transformationSystem)
-        val rotatingNode = RotatingNode()
-        rotatingNode.renderable = renderable
-        rotatingNode.addChild(transformableNode)
-        rotatingNode.setParent(anchorNode)
-
-        //transformableNode.renderable = renderable
-        //transformableNode.setParent(anchorNode)
-
-        fragment.arSceneView.scene.addChild(anchorNode)
-        transformableNode.select()
-    }
-
-
-    private fun getScreenCenter(): android.graphics.Point {
-        val vw = findViewById<View>(android.R.id.content)
-        return Point(vw.width / 2, vw.height / 2)
-    }
-
-
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
